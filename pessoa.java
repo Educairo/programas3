fe080521c42f44bfa6abb5ab96bb3c6c1f9a5da0
Educairo <102773100+Educairo@users.noreply.github.com>
2022-05-14 21:06:51 -0300

    Add files via upload

---
+++ pessoa.java
@@ -0,0 +1,26 @@
+public class pessoa {
+    private string nome;
+    private int idade = 18;
+
+    public void setNome(String nome){
+        this.nome = nome;
+
+        public void setIdade(int idade);{
+            if (idade>= 18)
+            this.idade = idade;
+         else
+         this.idade=18;
+
+            public String getNome(){
+                return nome
+
+                public int get idade(){
+                    return idade;
+                }
+            }
+        
+
+    }
+
+    
+}
